c13ac48466829a5b1c2fcec77ed4c6e13f108530
Maria Eduarda <mariaeduardamartinsf@gmail.com>
2025-09-30 10:56:17 -0300

    Update Home component to fetch and display user data from GitHub API after a 2-second delay.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <h1>hello world</h1>;
+export default async function Home() {
+  //falmos que queremos chamar a funcao resolve por 2 segundos
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch("https://api.github.com/users/mariaemf");
+  const data = await response.json();
+  console.log(data);
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
